4eab5cb5d40695a8643d0bd7e9e5cc354a6c80ff
Naringe, Prajakta <PRAJAKTA.NARINGE@asurion.com>
2020-11-25 11:08:15 +0530

    Added proper error message on UI

--- app/src/main/java/com/i/vetrinarykotlinapp/activity/MainActivity.kt
+++ app/src/main/java/com/i/vetrinarykotlinapp/activity/MainActivity.kt
@@ -10,10 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.i.vetrinarykotlinapp.Constants
-import com.i.vetrinarykotlinapp.Injection
-import com.i.vetrinarykotlinapp.R
-import com.i.vetrinarykotlinapp.WebRepository
+import com.i.vetrinarykotlinapp.*
 import com.i.vetrinarykotlinapp.utils.Util
 import com.i.vetrinarykotlinapp.adapter.PetAdapter
 import com.i.vetrinarykotlinapp.model.Pet
@@ -26,6 +23,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnChat: Button
     private lateinit var btnCall: Button
     private lateinit var textViewWorkingHours: TextView
+    private lateinit var textViewErrorMessage: TextView
+
     private lateinit var configViewModel: ConfigViewModel
     private lateinit var petListDetails: RecyclerView
     private lateinit var webRepository: WebRepository
@@ -41,6 +40,7 @@ class MainActivity : AppCompatActivity() {
         btnCall = findViewById(R.id.btn_call)
         textViewWorkingHours = findViewById(R.id.textView_officeHrs)
         petListDetails = findViewById(R.id.recyclerView)
+        textViewErrorMessage = findViewById(R.id.error_message)
         this.webRepository = WebRepository(Injection.okHttpClient)
 
         configViewModel = ViewModelProvider(
@@ -78,21 +78,26 @@ class MainActivity : AppCompatActivity() {
     private fun setConfigDetails() {
 
         configViewModel.configs?.observe(this, {
-            if (it.data?.get(0)?.isChatEnabled!! && it.data[0].isCallEnabled) {
-                btnChat.visibility = VISIBLE
-                btnCall.visibility = VISIBLE
-            } else if (!it.data[0].isChatEnabled && !it.data[0].isCallEnabled) {
-                btnChat.visibility = GONE
-                btnCall.visibility = GONE
-            } else if (it.data[0].isChatEnabled) {
-                btnChat.visibility = VISIBLE
-                btnCall.visibility = GONE
-            } else if (it.data[0].isCallEnabled) {
-                btnChat.visibility = GONE
-                btnCall.visibility = VISIBLE
+            if (it.status == Resource.Status.SUCCESS) {
+                Util.visibility(textViewWorkingHours, textViewErrorMessage)
+                if (it.data?.get(0)?.isChatEnabled!! && it.data[0].isCallEnabled) {
+                    btnChat.visibility = VISIBLE
+                    btnCall.visibility = VISIBLE
+                } else if (!it.data[0].isChatEnabled && !it.data[0].isCallEnabled) {
+                    btnChat.visibility = GONE
+                    btnCall.visibility = GONE
+                } else if (it.data[0].isChatEnabled) {
+                    btnChat.visibility = VISIBLE
+                    btnCall.visibility = GONE
+                } else if (it.data[0].isCallEnabled) {
+                    btnChat.visibility = GONE
+                    btnCall.visibility = VISIBLE
+                }
+                textViewWorkingHours.text =
+                    this.getString(R.string.office_hrs, it.data[0].workHours)
+            } else {
+                Util.visibilityGone(textViewWorkingHours, textViewErrorMessage)
             }
-            textViewWorkingHours.text =
-                this.getString(R.string.office_hrs, it.data[0].workHours)
         })
     }
 
@@ -103,13 +108,21 @@ class MainActivity : AppCompatActivity() {
     private fun setPetDetails() {
 
         configViewModel.pets?.observe(this, { petlist ->
-            petListDetails.layoutManager = LinearLayoutManager(application)
-            val adapter = PetAdapter(petlist.data as ArrayList<Pet>) { item ->
-                val intent = Intent(applicationContext, WebViewActivity::class.java)
-                intent.putExtra(Constants.CONTENT_URL, item.contentUrl)
-                startActivity(intent)
+            if (petlist.status == Resource.Status.SUCCESS) {
+                Util.visibility(textViewWorkingHours, textViewErrorMessage)
+                petListDetails.layoutManager = LinearLayoutManager(application)
+                val adapter = PetAdapter(petlist.data as ArrayList<Pet>) { item ->
+                    val intent = Intent(applicationContext, WebViewActivity::class.java)
+                    intent.putExtra(Constants.CONTENT_URL, item.contentUrl)
+                    startActivity(intent)
+                }
+                petListDetails.adapter = adapter
+            } else {
+                Util.visibilityGone(textViewWorkingHours, textViewErrorMessage)
+
+
             }
-            petListDetails.adapter = adapter
         })
+
     }
 }
--- app/src/main/java/com/i/vetrinarykotlinapp/utils/Util.kt
+++ app/src/main/java/com/i/vetrinarykotlinapp/utils/Util.kt
@@ -8,7 +8,9 @@ import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.util.LruCache
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import com.i.vetrinarykotlinapp.R
 import java.net.HttpURLConnection
@@ -38,6 +40,7 @@ object Util {
             }
         }
     }
+
     /**
      * This method is to display Image from imageUrl in imageView
      */
@@ -65,11 +68,13 @@ object Util {
             }
         }
     }
+
     private fun updateImageView(imageView: ImageView, bitmap: Bitmap) {
         uiHandler.post {
             imageView.setImageBitmap(bitmap)
         }
     }
+
     private fun downloadImage(mainUrl: String): Bitmap? {
         var bitmap: Bitmap? = null
         try {
@@ -83,6 +88,7 @@ object Util {
 
         return bitmap
     }
+
     /**
      * This method gives Alert Message with OK button
      */
@@ -94,6 +100,7 @@ object Util {
         ) { dialog, which -> dialog.dismiss() }
         dialog.show()
     }
+
     /**
      * This method is to match the current time with working time
      */
@@ -112,6 +119,7 @@ object Util {
         }
         return false
     }
+
     /**
      * This Method is compare start time and end time
      */
@@ -133,6 +141,7 @@ object Util {
         return false
 
     }
+
     /**
      * This method is to check current date is weekend or not
      */
@@ -141,5 +150,17 @@ object Util {
         val date: LocalDate = LocalDate.now()
         val day: DayOfWeek = DayOfWeek.of(date.get(ChronoField.DAY_OF_WEEK))
         return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY
+    }
+
+    fun visibilityGone(textViewWorkingHours: TextView, textViewErrorMessage: TextView) {
+        textViewWorkingHours.visibility = View.GONE
+        textViewErrorMessage.visibility = View.VISIBLE
+
+    }
+
+    fun visibility(textViewWorkingHours: TextView, textViewErrorMessage: TextView) {
+        textViewWorkingHours.visibility = View.VISIBLE
+        textViewErrorMessage.visibility = View.GONE
+
     }
 }
